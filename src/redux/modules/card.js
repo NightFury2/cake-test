eec56e22d7bf1fc82c1664d9278f711661056bb8
nightfury <lunvvova42@gmail.com>
2017-03-06 14:39:52 +0300

    add new algo to cake card system

--- src/redux/modules/card.js
+++ src/redux/modules/card.js
@@ -1,12 +1,6 @@
 // Id
 const ADD_CAKE_INFO_TO_PRICE = 'ADD_CAKE_INFO_TO_PRICE';
 
-
-const SET_CAKE_SIZE_ID = 'SET_CAKE_SIZE_ID';
-const SET_CAKE_FILLING_ID = 'SET_CAKE_FILLING_ID';
-const SET_CAKE_MOTIVE_PRICE = 'SET_CAKE_MOTIVE_PRICE';
-const SET_CAKE_TEXT_PRICE = 'SET_CAKE_TEXT_PRICE';
-const SET_CAKE_EXTRA_PRICE = 'SET_CAKE_EXTRA_PRICE';
 const SET_CAKE_MESSAGE_BAKERY = 'SET_CAKE_MESSAGE_BAKERY';
 const SET_CAKE_COUNT = 'SET_CAKE_COUNT';
 const SET_CAKE_DEFAULT = 'SET_CAKE_DEFAULT';
@@ -20,23 +14,7 @@ const initialState = {
   cakeSizeId: 0,
   cakeFillingId: 0,
   count: 1,
-  listCakePrice: [],
-  cakeMotivePrice: {
-    files: [],
-    price: 0
-  },
-  cakeTextPrice: {
-    text: '',
-    price: 0
-  },
-  extraFillPrice: {
-    cakeExtensionId: 0,
-    name: '',
-    price: 0
-  },
-  messageBakery: {
-    text: ''
-  },
+  cakePriceLists: [],
   step: {
     cakeSizeStep: true,
     cakeFillingStep: false,
@@ -53,42 +31,13 @@ export default function cakes(state = initialState, action = {}) {
         cakeSizeId: initialState.cakeSizeId,
         cakeFillingId: initialState.cakeFillingId,
         count: initialState.count,
-        cakeMotivePrice: initialState.cakeMotivePrice,
-        cakeTextPrice: initialState.cakeTextPrice,
-        extraFillPrice: initialState.extraFillPrice,
-        messageBakery: initialState.messageBakery,
+        cakePriceLists: [],
         step: initialState.step
       };
     case ADD_CAKE_INFO_TO_PRICE:
       return {
         ...state,
-        listCakePrice: action.listCakePrice
-      };
-    // set Id
-    case SET_CAKE_SIZE_ID:
-      return {
-        ...state,
-        cakeSizeId: action.cakeSizeId
-      };
-    case SET_CAKE_FILLING_ID:
-      return {
-        ...state,
-        cakeFillingId: action.cakeFillingId
-      };
-    case SET_CAKE_MOTIVE_PRICE:
-      return {
-        ...state,
-        cakeMotivePrice: action.cakeMotivePrice
-      };
-    case SET_CAKE_TEXT_PRICE:
-      return {
-        ...state,
-        cakeTextPrice: action.cakeTextPrice
-      };
-    case SET_CAKE_EXTRA_PRICE:
-      return {
-        ...state,
-        extraFillPrice: action.extraFillPrice
+        cakePriceLists: action.cakePriceList
       };
     case SET_CAKE_MESSAGE_BAKERY:
       return {
@@ -146,31 +95,55 @@ export function setCakeDefault() {
   return {type: SET_CAKE_DEFAULT};
 }
 
-export function addToListCakePrice(listCakePrices, cakeInfo) {
-  const listCakePrice = listCakePrices.push(cakeInfo);
-  return {type: SET_CAKE_DEFAULT,  listCakePrice};
+
+// Add New Cake
+export function addNewCakeToPriceList(cakePriceLists, newCake) {
+  const cakePriceList = cakePriceLists.push(newCake);
+  return {type: SET_CAKE_DEFAULT,  cakePriceList};
 }
 
-export function setCakeFillingId(cakeFillingId) {
-  return {type: SET_CAKE_FILLING_ID, cakeFillingId};
+// Add CakeSize
+export function addCakeSizeToCake(cakePriceLists, cakeSize) {
+  // TODO
+  // return {type: SET_CAKE_DEFAULT};
 }
 
-export function setCakeMotivePrice(cakeMotivePrice) {
-  return {type: SET_CAKE_MOTIVE_PRICE, cakeMotivePrice};
+// Add CakeFilling
+export function addCakeFillingToCake(cakePriceLists, cakeFilling) {
+  // TODO
+  // return {type: SET_CAKE_DEFAULT};
 }
-export function setCakeExtaFillPrice(extraFillPrice) {
-  return {type: SET_CAKE_EXTRA_PRICE, extraFillPrice};
+
+// Add CakeMotive
+export function addCakeMotiveToCake(cakePriceLists, cakeMotive) {
+  // TODO
+  // return {type: SET_CAKE_DEFAULT};
 }
-export function setCakeCount(count) {
-  return {type: SET_CAKE_COUNT, count};
+
+// Add CakeText
+export function addCakeTextToCake(cakePriceLists, cakeText) {
+  // TODO
+  // return {type: SET_CAKE_DEFAULT};
 }
-export function setCakeMessageBakery(messageBakery) {
-  return {type: SET_CAKE_MESSAGE_BAKERY, messageBakery};
+
+// Add CakeExtension
+export function addCakeExtensionToCake(cakePriceLists, cakeExtension) {
+  // TODO
+  // return {type: SET_CAKE_DEFAULT};
 }
-export function setCakeTextPrice(cakeTexBytPrice, price) {
-  const cakeTextPrice = cakeTexBytPrice.length > 0 ? {text: cakeTexBytPrice, price: price} : {text: '', price: 0};
-  return {type: SET_CAKE_TEXT_PRICE, cakeTextPrice};
+
+// Add CakeMessageBakery
+export function addCakeMessageBakeryToCake(cakePriceLists, cakeMessageBakery) {
+  // TODO
+  // return {type: SET_CAKE_DEFAULT};
 }
+
+// Add CakeCount
+export function addCakeCountBakeryToCake(cakePriceLists, cakeCount) {
+  // TODO
+  // return {type: SET_CAKE_DEFAULT};
+}
+
 // steps
 export function setCakeSizeStep(isSizeStep) {
   return {type: SET_CAKE_SIZE_STEP, isSizeStep};
